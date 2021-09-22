32838adc984c22f2e391b5ce0526ea98fa2fff95
enpasos <matthias.unverzagt@enpasos.com>
2021-09-22 16:22:05 +0200

    some refactorings
    Former-commit-id: 0b135da6655364668cc1547bacd220c77c525571

--- src/test/java/ai/enpasos/muzero/environments/go/environment/scoring/MoreScoringTest.java
+++ src/test/java/ai/enpasos/muzero/environments/go/environment/scoring/MoreScoringTest.java
@@ -2,14 +2,12 @@ package ai.enpasos.muzero.environments.go.environment.scoring;
 
 
 import ai.enpasos.muzero.environments.go.environment.GoBoard;
-import ai.enpasos.muzero.environments.go.environment.Point;
+import ai.enpasos.muzero.environments.go.environment.basics.Point;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
-import static ai.enpasos.muzero.environments.go.environment.Player.BlackPlayer;
-import static ai.enpasos.muzero.environments.go.environment.Player.WhitePlayer;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static ai.enpasos.muzero.environments.go.environment.basics.Player.BlackPlayer;
+import static ai.enpasos.muzero.environments.go.environment.basics.Player.WhitePlayer;
 
 @Slf4j
 public class MoreScoringTest {
@@ -57,6 +55,101 @@ public class MoreScoringTest {
         log.debug("result = \n" + result.toDebugString());
 
     }
+
+
+    // 5  X  X  O  O  .
+    // 4  .  X  X  O  X
+    // 3  X  X  X  O  X
+    // 2  X  X  O  .  O
+    // 1  X  O  O  O  .
+    //    A  B  C  D  E
+    @Test
+    void scoreAGiven5x5GameE() {
+        var board = new GoBoard(5);
+
+        board = board.placeStone(BlackPlayer, new Point(1, 1));
+        board = board.placeStone(BlackPlayer, new Point(1, 2));
+        board = board.placeStone(WhitePlayer, new Point(1, 3));
+        board = board.placeStone(WhitePlayer, new Point(1, 4));
+
+        board = board.placeStone(BlackPlayer, new Point(2, 2));
+        board = board.placeStone(BlackPlayer, new Point(2, 3));
+        board = board.placeStone(WhitePlayer, new Point(2, 4));
+        board = board.placeStone(BlackPlayer, new Point(2, 5));
+
+
+        board = board.placeStone(BlackPlayer, new Point(3, 1));
+        board = board.placeStone(BlackPlayer, new Point(3, 2));
+        board = board.placeStone(BlackPlayer, new Point(3, 3));
+        board = board.placeStone(WhitePlayer, new Point(3, 4));
+        board = board.placeStone(BlackPlayer, new Point(3, 5));
+
+
+        board = board.placeStone(BlackPlayer, new Point(4, 1));
+        board = board.placeStone(BlackPlayer, new Point(4, 2));
+        board = board.placeStone(WhitePlayer, new Point(4, 3));
+        board = board.placeStone(WhitePlayer, new Point(4, 5));
+
+        board = board.placeStone(BlackPlayer, new Point(5, 1));
+        board = board.placeStone(WhitePlayer, new Point(5, 2));
+        board = board.placeStone(WhitePlayer, new Point(5, 3));
+        board = board.placeStone(WhitePlayer, new Point(5, 4));
+
+        log.debug("final board configuration: \n" + board);
+
+        var result = GameResult.apply(board);
+        log.debug("result = \n" + result.toString());
+        log.debug("result = \n" + result.toDebugString());
+
+    }
+
+
+
+    // 5  .  O  .  O  .
+    // 4  O  O  O  X  X
+    // 3  .  O  X  X  X
+    // 2  O  O  X  X  .
+    // 1  .  X  .  X  X
+    //    A  B  C  D  E
+    @Test
+    void scoreAGiven5x5GameD() {
+        var board = new GoBoard(5);
+
+        board = board.placeStone(WhitePlayer, new Point(1, 2));
+        board = board.placeStone(WhitePlayer, new Point(1, 4));
+
+        board = board.placeStone(WhitePlayer, new Point(2, 1));
+        board = board.placeStone(WhitePlayer, new Point(2, 2));
+        board = board.placeStone(WhitePlayer, new Point(2, 3));
+        board = board.placeStone(BlackPlayer, new Point(2, 4));
+        board = board.placeStone(BlackPlayer, new Point(2, 5));
+
+
+        board = board.placeStone(WhitePlayer, new Point(3, 2));
+        board = board.placeStone(BlackPlayer, new Point(3, 3));
+        board = board.placeStone(BlackPlayer, new Point(3, 4));
+        board = board.placeStone(BlackPlayer, new Point(3, 5));
+
+
+        board = board.placeStone(WhitePlayer, new Point(4, 1));
+        board = board.placeStone(WhitePlayer, new Point(4, 2));
+        board = board.placeStone(BlackPlayer, new Point(4, 3));
+        board = board.placeStone(BlackPlayer, new Point(4, 4));
+
+        board = board.placeStone(BlackPlayer, new Point(5, 2));
+        board = board.placeStone(BlackPlayer, new Point(5, 4));
+        board = board.placeStone(BlackPlayer, new Point(5, 5));
+
+        log.debug("final board configuration: \n" + board);
+
+        var result = GameResult.apply(board);
+        log.debug("result = \n" + result.toString());
+        log.debug("result = \n" + result.toDebugString());
+
+    }
+
+
+
 
 
     // 5  X  X  O  O  .
