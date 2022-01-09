9d364ca063497d805165cfe9af0010de4f80980f
enpasos <info@enpasos.com>
2022-01-09 21:35:24 +0100

    scaling korrigiert

--- platform/src/main/java/ai/enpasos/muzero/platform/agent/fast/model/djl/blocks/dlowerlevel/RescaleBlock.java
+++ platform/src/main/java/ai/enpasos/muzero/platform/agent/fast/model/djl/blocks/dlowerlevel/RescaleBlock.java
@@ -42,12 +42,21 @@ public class RescaleBlock extends AbstractBlock {
 
 
         // scale on each  ... a pytorch view or multi-dim support would be better
+//        Shape origShape = current.getShape();
+//        Shape shape2 = new Shape(origShape.get(0), origShape.get(1), origShape.get(2) * origShape.get(3));
+//        NDArray current2 = current.reshape(shape2);
+//        Shape shape3 = new Shape(origShape.get(0), origShape.get(1), 1, 1);
+//        NDArray min2 = current2.min(new int[]{2}, true).reshape(shape3);
+//        NDArray max2 = current2.max(new int[]{2}, true).reshape(shape3);
+
+        // alternative global min, max
+
         Shape origShape = current.getShape();
-        Shape shape2 = new Shape(origShape.get(0), origShape.get(1), origShape.get(2) * origShape.get(3));
+        Shape shape2 = new Shape(origShape.get(0), origShape.get(1) * origShape.get(2) * origShape.get(3));
         NDArray current2 = current.reshape(shape2);
-        Shape shape3 = new Shape(origShape.get(0), origShape.get(1), 1, 1);
-        NDArray min2 = current2.min(new int[]{2}, true).reshape(shape3);
-        NDArray max2 = current2.max(new int[]{2}, true).reshape(shape3);
+        Shape shape3 = new Shape(origShape.get(0), 1, 1, 1);
+        NDArray min2 = current2.min(new int[]{1}, true).reshape(shape3);
+        NDArray max2 = current2.max(new int[]{1}, true).reshape(shape3);
 
         NDArray d = max2.sub(min2).maximum(1e-5);
         NDArray a = current.sub(min2);
