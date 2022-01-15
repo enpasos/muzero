e6a65efd2c2d8e92e2ed744a4fc3fc6f6585c932
enpasos <info@enpasos.com>
2022-01-15 09:12:48 +0100

    intermediate

--- onnxModelGen/src/main/java/ai/enpasos/mnist/blocks/ext/LayerNormExt.java
+++ onnxModelGen/src/main/java/ai/enpasos/mnist/blocks/ext/LayerNormExt.java
@@ -1,6 +1,7 @@
 package ai.enpasos.mnist.blocks.ext;
 
 import ai.djl.ndarray.NDArray;
+import ai.djl.ndarray.types.Shape;
 import ai.djl.nn.norm.LayerNormOpened;
 import ai.enpasos.mnist.blocks.OnnxBlock;
 import ai.enpasos.mnist.blocks.OnnxCounter;
@@ -18,188 +19,216 @@ import static ai.enpasos.mnist.blocks.OnnxHelper.createValueInfoProto;
 
 public class LayerNormExt extends LayerNormOpened implements OnnxIO {
 
-
+/*
+ * Workaround at least to make inference work for onnx
+ * see https://github.com/onnx/onnx/issues/2379
+ */
     LayerNormExt(LayerNormExt.Builder builder) {
         super(builder);
     }
 
-    /**
-     * Creates a builder to build a {@code LayerNorm}.
-     *
-     * @return a new builder
-     */
+
     public static Builder builder() {
         return new Builder();
     }
 
     @Override
     public OnnxBlock getOnnxBlock(OnnxCounter counter, List<OnnxTensor> input) {
+        Shape inputShape = input.get(0).getShape();
+        Shape condensedShape = new Shape(inputShape.get(0),inputShape.get(1) * inputShape.get(2) * inputShape.get(3));
+        Shape fullyCondensedShape = new Shape(inputShape.get(0) * inputShape.get(1) * inputShape.get(2) * inputShape.get(3));
 
 
-        OnnxBlock blockRandomLike = nodeRandomLike(counter, input);
-        OnnxBlock blockNodeAdd = nodeAdd(counter, input, blockRandomLike.getOutput());
+        OnnxBlock blockReshape = nodeReshape(counter, input, condensedShape);
+        OnnxBlock blockTranspose = nodeTranspose(counter,  blockReshape.getOutput());
 
-        OnnxBlock blockMVN = nodeMVN(counter, blockNodeAdd.getOutput());
-        OnnxBlock blockMul = nodeMul(counter, blockMVN.getOutput());
-        OnnxBlock blockAddBeta = nodeAddBeta(counter, blockMul.getOutput());
+        String betaName = "beta" + counter.count();
+        NDArray beta = this.parameters.get("beta").getArray();   // transpose??
+        String gammaName = "gamma" + counter.count();
+        NDArray gamma = this.parameters.get("gamma").getArray();  // transpose??
+
+        OnnxBlock blockDataGamma = nodeData(counter, gamma, fullyCondensedShape);
+        OnnxBlock blockDataBeta = nodeData(counter, beta, fullyCondensedShape);
+
+
+
+        OnnxBlock blockBatchNormalization = nodeBatchNormalization(counter, blockTranspose.getOutput(), blockDataGamma.getOutput(), blockDataBeta.getOutput(), beta);
+        OnnxBlock blockTranspose2 = nodeTranspose(counter,  blockBatchNormalization.getOutput());
+        OnnxBlock blockReshape2 = nodeReshape(counter, blockTranspose2.getOutput(), inputShape);
+
+
 
         OnnxBlock onnxBlock = OnnxBlock.builder()
             .input(input)
             .valueInfos(createValueInfoProto(input))
-            .output(blockAddBeta.getOutput())
+            .output(blockReshape2.getOutput())
             .build();
 
-        onnxBlock.getValueInfos().addAll(createValueInfoProto(blockAddBeta.getOutput()));
-        onnxBlock.addChild(blockMVN);
-        onnxBlock.addChild(blockMul);
-        onnxBlock.addChild(blockAddBeta);
-        onnxBlock.addChild(blockRandomLike);
-        onnxBlock.addChild(blockNodeAdd);
+        onnxBlock.addChild(blockReshape);
+        onnxBlock.addChild(blockTranspose);
+        onnxBlock.addChild(blockDataGamma);
+        onnxBlock.addChild(blockDataBeta);
+        onnxBlock.addChild(blockBatchNormalization);
+        onnxBlock.addChild(blockTranspose2);
+        onnxBlock.addChild(blockReshape2);
 
         return onnxBlock;
 
     }
 
 
-  private OnnxBlock nodeRandomLike(OnnxCounter counter, List<OnnxTensor> input) {
+  private OnnxBlock nodeReshape(OnnxCounter counter, List<OnnxTensor> input, Shape targetShape) {
       List<OnnxTensor> output = combine(
           List.of("T" + counter.count()),
-          List.of(input.get(0).getShape())
+          List.of(targetShape)
       );
+      String shapeName = "T" + counter.count();
       return OnnxBlock.builder()
+          .input(input)
           .output(output)
           .valueInfos(createValueInfoProto(output))
           .nodes(List.of(
               NodeProto.newBuilder()
                   .setName("Node" + counter.count())
-                  .setOpType("RandomUniformLike")
-                  .addAttribute(AttributeProto.newBuilder()
-                      .setType(AttributeProto.AttributeType.FLOAT)
-                      .setName("low")
-                      .setF(-1e-7f)
-                      .build())
-                  .addAttribute(AttributeProto.newBuilder()
-                      .setType(AttributeProto.AttributeType.FLOAT)
-                      .setName("high")
-                      .setF(1e-7f)
-                      .build())
+                  .setOpType("Reshape")
                   .addInput(input.get(0).getName())
+                  .addInput(shapeName)
                   .addOutput(output.get(0).getName())
                   .build()
           ))
-          .valueInfos(createValueInfoProto(output))
+          .parameters(List.of(
+              TensorProto.newBuilder()
+                  .setName(shapeName)
+                  .setDataType(TensorProto.INT64_DATA_FIELD_NUMBER)
+                  .addAllDims(List.of(2L))
+                  .addAllInt64Data(convert(targetShape.getShape()))
+                  .build()
+          ))
           .build();
   }
 
-    private OnnxBlock nodeMVN(OnnxCounter counter, List<OnnxTensor> input) {
+    private OnnxBlock nodeTranspose(OnnxCounter counter, List<OnnxTensor> input) {
+        Shape inputShape = input.get(0).getShape();
+        List<OnnxTensor> output = combine(
+            List.of("T" + counter.count()),
+            List.of(new Shape(inputShape.get(1), inputShape.get(0)))
+        );
+        String shapeName = "T" + counter.count();
+        return OnnxBlock.builder()
+            .input(input)
+            .output(output)
+            .valueInfos(createValueInfoProto(output))
+            .nodes(List.of(
+                NodeProto.newBuilder()
+                    .setName("Node" + counter.count())
+                    .setOpType("Transpose")
+//                    .addAttribute(AttributeProto.newBuilder()
+//                        .setType(AttributeProto.AttributeType.INTS)
+//                        .setName("perm")
+//                        .addAllInts(List.of(1L, 0L))
+//                        .build())
+                    .addInput(input.get(0).getName())
+                    .addOutput(output.get(0).getName())
+                    .build()
+            ))
+
+            .build();
+    }
+
+    private OnnxBlock nodeBatchNormalization(OnnxCounter counter, List<OnnxTensor> input,List<OnnxTensor> gamma,List<OnnxTensor> beta, NDArray betaData) {
         List<OnnxTensor> output = combine(
             List.of("T" + counter.count()),
             List.of(input.get(0).getShape())
         );
+
+        String dummyRunningMean = "T" + counter.count();
+        String dummyRunningVar = "T" + counter.count();
         return OnnxBlock.builder()
             .output(output)
             .valueInfos(createValueInfoProto(output))
             .nodes(List.of(
                 NodeProto.newBuilder()
                     .setName("Node" + counter.count())
-                    .setOpType("MeanVarianceNormalization")
+                    .setOpType("BatchNormalization")
                     .addAttribute(AttributeProto.newBuilder()
-                        .setType(AttributeProto.AttributeType.INTS)
-                        .setName("axes")
-                        .addAllInts(List.of(1L, 2L, 3L))
+                        .setType(AttributeProto.AttributeType.FLOAT)
+                        .setName("momentum")   // do not use running_mean
+                        .setF(0f)
+                        .build())
+                    .addAttribute(AttributeProto.newBuilder()
+                        .setType(AttributeProto.AttributeType.INT)
+                        .setName("training_mode")
+                        .setI(0)   // default
                         .build())
                     .addInput(input.get(0).getName())
-                    .addOutput(output.get(0).getName())
-                    .build()
-            ))
-            .valueInfos(createValueInfoProto(output))
-            .build();
-    }
-
-    private OnnxBlock nodeMul(OnnxCounter counter, List<OnnxTensor> input) {
-        List<OnnxTensor> output = combine(
-            List.of("T" + counter.count()),
-            List.of(input.get(0).getShape())
-        );
-        String gammaName = "gamma" + counter.count();
-        NDArray gamma = this.parameters.get("gamma").getArray();
-        return OnnxBlock.builder()
-            .output(output)
-            .valueInfos(createValueInfoProto(output))
-            .nodes(List.of(
-                NodeProto.newBuilder()
-                    .setName("N" + counter.count())
-                    .setOpType("Mul")
-                    .addInput(input.get(0).getName())
-                    .addInput(gammaName)
+                    .addInput(gamma.get(0).getName())
+                    .addInput(beta.get(0).getName())
+                    .addInput(dummyRunningMean)
+                    .addInput(dummyRunningVar)
                     .addOutput(output.get(0).getName())
                     .build()
             ))
             .parameters(List.of(
                 TensorProto.newBuilder()
-                    .setName(gammaName)
+                    .setName(dummyRunningMean)
                     .setDataType(1)
-                    .addAllDims(convert(gamma.getShape().getShape()))
-                    .addAllFloatData(convert(gamma))
+                    .addAllDims(convert(beta.get(0).getShape().getShape()))
+                    .addAllFloatData(  convert(betaData.toFloatArray() ))
+                    .build(),
+                TensorProto.newBuilder()
+                    .setName(dummyRunningVar)
+                    .setDataType(1)
+                    .addAllDims(convert(beta.get(0).getShape().getShape()))
+                    .addAllFloatData(  convert(betaData.toFloatArray() ))
                     .build()
             ))
-            .valueInfos(createValueInfoProto(output))
             .build();
     }
-    private OnnxBlock nodeAdd(OnnxCounter counter, List<OnnxTensor> inputA, List<OnnxTensor> inputB) {
-        List<OnnxTensor> output = combine(
-            List.of("T" + counter.count()),
-            List.of(inputA.get(0).getShape())
-        );
-        String betaName = "beta" + counter.count();
-        NDArray beta = this.parameters.get("beta").getArray();
-        return OnnxBlock.builder()
-            .output(output)
-            .valueInfos(createValueInfoProto(output))
-            .nodes(List.of(
-                NodeProto.newBuilder()
-                    .setName("N" + counter.count())
-                    .setOpType("Add")
-                    .addInput(inputA.get(0).getName())
-                    .addInput(inputB.get(0).getName())
-                    .addOutput(output.get(0).getName())
-                    .build()
-            ))
-            .valueInfos(createValueInfoProto(output))
-            .build();
 
-    }
-    private OnnxBlock nodeAddBeta(OnnxCounter counter, List<OnnxTensor> input) {
-        List<OnnxTensor> output = combine(
-            List.of("T" + counter.count()),
-            List.of(input.get(0).getShape())
-        );
-        String betaName = "beta" + counter.count();
-        NDArray beta = this.parameters.get("beta").getArray();
+
+    private OnnxBlock nodeData(OnnxCounter ctx, NDArray data, Shape outputShape) { //}, onnxBlock onnxBlock, String outputName, Shape shape) {
+        List<OnnxTensor> output =  combine(List.of("T" + ctx.count()), List.of(outputShape));
+
+      //  NDArray weight = this.parameters.get("weight").getArray().transpose();
+
+        String parameterName1 = "P"+ ctx.count();
+        String parameterName2 = "P"+ ctx.count();
+
         return OnnxBlock.builder()
             .output(output)
             .valueInfos(createValueInfoProto(output))
             .nodes(List.of(
                 NodeProto.newBuilder()
-                    .setName("N" + counter.count())
-                    .setOpType("Add")
-                    .addInput(input.get(0).getName())
-                    .addInput(betaName)
+                    .setName("Node" + ctx.count())
+                    .setOpType("Reshape")
+                    .addInput(parameterName1)
+                    .addInput(parameterName2)
                     .addOutput(output.get(0).getName())
                     .build()
             ))
             .parameters(List.of(
+                // data
                 TensorProto.newBuilder()
-                    .setName(betaName)
+                    .setName(parameterName1)
                     .setDataType(1)
-                    .addAllDims(convert(beta.getShape().getShape()))
-                    .addAllFloatData(convert(beta))
+                    .addAllDims(convert(data.getShape().getShape()))
+                    .addAllFloatData(convert(data))
+                    .build(),
+                // shape
+                TensorProto.newBuilder()
+                    .setName(parameterName2)
+                    .setDataType(TensorProto.INT64_DATA_FIELD_NUMBER)
+                    .addAllDims(List.of(1L))
+                    .addAllInt64Data(convert(outputShape.getShape()))
                     .build()
             ))
-            .valueInfos(createValueInfoProto(output))
+            .valueInfos(
+                createValueInfoProto(output)
+            )
             .build();
 
     }
+
 
 
     public static final class Builder extends LayerNormOpened.Builder {
