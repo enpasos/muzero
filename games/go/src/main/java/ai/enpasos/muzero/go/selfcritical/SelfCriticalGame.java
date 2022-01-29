c4c002a93ebaff7a9b887878e4938d9e52f36ab3
enpasos <info@enpasos.com>
2022-01-29 08:23:24 +0100

    moved immature stuff to the branch selfcritical

--- games/go/src/main/java/ai/enpasos/muzero/go/selfcritical/SelfCriticalGame.java
+++
@@ -1,9 +0,0 @@
-package ai.enpasos.muzero.go.selfcritical;
-
-
-import java.util.TreeMap;
-
-public class SelfCriticalGame {
-   TreeMap<SelfCriticalPosition, Float> normalizedEntropyValues = new TreeMap<>();
-    int firstReliableFullMove;
-}
