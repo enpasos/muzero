da3da7e0278798af651013a8bc7c5223e0d33e0e
enpasos <info@enpasos.com>
2022-06-07 10:46:20 +0200

    MUZ-7 #comment bug fix for storing replayed games

--- platform/src/main/java/ai/enpasos/muzero/platform/run/Surprise.java
+++ platform/src/main/java/ai/enpasos/muzero/platform/run/Surprise.java
@@ -79,6 +79,7 @@ public class Surprise {
             newGame.getGameDTO().setTStateB(newGame.getGameDTO().getTStateA());
             newGame.getGameDTO().setTSurprise(0);
             newGame.getGameDTO().setSurprised(false);
+            newGame.getGameDTO().setNetworkName(replayBuffer.getCurrentNetworkName());
             gameSeeds.add(newGame);
         });
         replayBuffer.removeGames(gamesWithOldSurprise);
