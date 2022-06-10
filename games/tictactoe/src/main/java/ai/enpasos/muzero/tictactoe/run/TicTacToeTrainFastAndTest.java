67f037ba95a412ba7bda158944c26533249e003e
enpasos <info@enpasos.com>
2022-06-10 09:18:15 +0200

    MUZ-15 #end-progress #comment fixed

---
+++ games/tictactoe/src/main/java/ai/enpasos/muzero/tictactoe/run/TicTacToeTrainFastAndTest.java
@@ -0,0 +1,32 @@
+package ai.enpasos.muzero.tictactoe.run;
+
+
+import ai.enpasos.muzero.platform.agent.memorize.ReplayBuffer;
+import ai.enpasos.muzero.platform.config.MuZeroConfig;
+import ai.enpasos.muzero.platform.run.train.MuZeroFast2;
+import ai.enpasos.muzero.platform.run.train.TrainParams;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Slf4j
+@Component
+public class TicTacToeTrainFastAndTest {
+
+    @Autowired
+    private MuZeroFast2 muZeroFast;
+
+    @SuppressWarnings("squid:S125")
+    public void run() {
+        //   rmDir(config.getOutputDir());
+
+        muZeroFast.train(TrainParams.builder()
+            // .afterTrainingHookIn(this::adjustKomi)
+            .withoutFill(true)
+            //  .afterSelfPlayHookIn((epoch, network) -> goSurprise.train(epoch, network))
+            .build());
+    }
+
+
+}
