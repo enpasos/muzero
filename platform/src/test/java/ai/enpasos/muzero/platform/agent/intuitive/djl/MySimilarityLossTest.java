ff7c5b5278c609af63b00c95f4bfa4f6376b25eb
enpasos <matthias.unverzagt@enpasos.com>
2022-11-07 10:59:55 +0100

    more stable
    Former-commit-id: 88d5132efdf72e51f3b343b086c95971baa47222

--- platform/src/test/java/ai/enpasos/muzero/platform/agent/intuitive/djl/MySimilarityLossTest.java
+++ platform/src/test/java/ai/enpasos/muzero/platform/agent/intuitive/djl/MySimilarityLossTest.java
@@ -26,7 +26,7 @@ class MySimilarityLossTest {
         prediction = prediction.reshape(shape);
 
 
-        assertEquals(0f, loss.evaluate(new NDList(label), new NDList(prediction)).getFloat());
+        assertEquals(4.0006638E-4f, loss.evaluate(new NDList(label), new NDList(prediction)).getFloat());
 
     }
 }
