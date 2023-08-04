0c70d02fcba70a05452129d632faf2ca477034ac
enpasos <info@enpasos.com>
2023-08-04 09:10:35 +0200

    exploitability - runs, but best response policy needs to exploit

--- games/tictactoe/src/main/java/ai/enpasos/muzero/tictactoe/run/TicTacToeTestAllNetworks.java
+++ games/tictactoe/src/main/java/ai/enpasos/muzero/tictactoe/run/TicTacToeTestAllNetworks.java
@@ -43,8 +43,8 @@ public class TicTacToeTestAllNetworks {
     public void run() {
 
 
-        int start = 20; // 288;
-        int stop = 20; // 288;
+        int start = 288; // 288;
+        int stop = 288; // 288;
 
         boolean onOptimalPathOnly = false;
 
