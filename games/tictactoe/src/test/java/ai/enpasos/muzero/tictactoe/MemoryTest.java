a498041049fab4533fd4bd64da5dc493c3b495ca
enpasos <info@enpasos.com>
2024-02-14 10:59:24 +0100

    bug fix + minor

--- games/tictactoe/src/test/java/ai/enpasos/muzero/tictactoe/MemoryTest.java
+++ games/tictactoe/src/test/java/ai/enpasos/muzero/tictactoe/MemoryTest.java
@@ -20,7 +20,6 @@ package ai.enpasos.muzero.tictactoe;
 import ai.enpasos.muzero.platform.agent.e_experience.Game;
 import ai.enpasos.muzero.platform.agent.e_experience.memory.EpisodeMemory;
 import ai.enpasos.muzero.platform.agent.e_experience.memory.EpisodeMemoryImpl;
-import ai.enpasos.muzero.platform.agent.e_experience.memory.ObservationStateNode;
 import ai.enpasos.muzero.platform.config.MuZeroConfig;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -52,41 +51,11 @@ class MemoryTest {
         Game game1 = gameFromActions( 1, 5, 0, 4, 7, 2, 8, 6 );
         Game game2 = gameFromActions( 1, 5, 0, 4, 7, 2, 8, 3 );
         episodeMemory.add(game1);
-//        assertArrayEquals(new boolean[]{true, true, true, true, true, true, true, true, true},
-//                episodeMemory.getLegalNotDeeplyVisitedActions(game1, 0));
-//        assertArrayEquals(new boolean[]{false, false, false, true, false, false, false, false, false},
-//                episodeMemory.getLegalNotDeeplyVisitedActions(game1, 7));
 
         episodeMemory.add(game2);
-     //   assertEquals(10, episodeMemory.getNumberOfStateNodes());
         assertEquals(2, episodeMemory.getNumberOfEpisodes());
 
-        assertEquals(new ObservationStateNode(game1,0), new ObservationStateNode(game2, 0));
-        assertEquals(new ObservationStateNode(game1,1), new ObservationStateNode(game2, 1));
-        assertEquals(new ObservationStateNode(game1,2), new ObservationStateNode(game2, 2));
-        assertEquals(new ObservationStateNode(game1,3), new ObservationStateNode(game2, 3));
-        assertEquals(new ObservationStateNode(game1,4), new ObservationStateNode(game2, 4));
-        assertNotEquals(new ObservationStateNode(game1,4), new ObservationStateNode(game2, 5));
-        assertEquals(new ObservationStateNode(game1,5), new ObservationStateNode(game2, 5));
-        assertEquals(new ObservationStateNode(game1,6), new ObservationStateNode(game2, 6));
-        assertEquals(new ObservationStateNode(game1,7), new ObservationStateNode(game2, 7));
-        assertNotEquals(new ObservationStateNode(game1,8), new ObservationStateNode(game2, 8));
-        assertEquals(new ObservationStateNode(game1,2).hashCode(), new ObservationStateNode(game2,2).hashCode());
 
-
-//        assertArrayEquals(new boolean[]{false, false, false, false, false, false, false, false, false},
-//                episodeMemory.getLegalNotDeeplyVisitedActions(game2, 7));
-//
-//
-//        assertArrayEquals(new boolean[]{false, false, false, false, false, false, false, false, false},
-//                episodeMemory.getLegalNotDeeplyVisitedActions(game1, 7));
-
-
-//        assertFalse(episodeMemory.visitsUnvisitedAction(game1));
-//        Game game3 = gameFromActions( 1, 5, 7, 4, 0, 2, 8, 3 );
-//        assertTrue(episodeMemory.visitsUnvisitedAction(game3));
-//        episodeMemory.add(game3);
-//        assertFalse(episodeMemory.visitsUnvisitedAction(game3));
 
         episodeMemory.add(gameFromActions( 1, 5, 0, 4, 2));
         episodeMemory.add(gameFromActions( 0, 5, 4, 3, 8));
@@ -94,10 +63,10 @@ class MemoryTest {
         episodeMemory.add(gameFromActions( 1, 2, 4, 3, 7));
         episodeMemory.add(gameFromActions( 3, 1, 4, 0, 5));
         episodeMemory.add(gameFromActions( 6, 1, 7, 0, 8));
-      //  assertEquals(9, episodeMemory.getNumberOfEpisodes());
+        assertEquals(8, episodeMemory.getNumberOfEpisodes());
         episodeMemory.setCapacity(5);
         episodeMemory.add(gameFromActions( 3, 1, 4, 0, 5));
-     //   assertEquals(5, episodeMemory.getNumberOfEpisodes());
+        assertEquals(5, episodeMemory.getNumberOfEpisodes());
 
     }
 
