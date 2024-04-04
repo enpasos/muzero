4f364d26a1d8410c28e6f4202f7f71b92b736c23
enpasos <info@enpasos.com>
2024-04-04 11:10:47 +0200

    tests are running again

--- platform/src/main/java/ai/enpasos/muzero/platform/agent/d_model/djl/blocks/c_mainfunctions/PredictionBlock.java
+++ platform/src/main/java/ai/enpasos/muzero/platform/agent/d_model/djl/blocks/c_mainfunctions/PredictionBlock.java
@@ -130,18 +130,6 @@ private boolean withReward;
 
     }
 
-//    @Override
-//    protected NDList forwardInternal(ParameterStore parameterStore, NDList inputs, boolean training, PairList<String, Object> params) {
-//        NDList results = new NDList();
-//
-//        results.add(this.legalActionsHead.forward(parameterStore, new NDList(inputs.get(0)), training, params).get(0));
-//        if (withReward) {
-//            results.add(this.rewardHead.forward(parameterStore, new NDList(inputs.get(0)), training, params).get(0));
-//        }
-//        results.add(this.policyHead.forward(parameterStore, new NDList(inputs.get(1)), training, params).get(0));
-//        results.add(this.valueHead.forward(parameterStore, new NDList(inputs.get(2)), training, params).get(0));
-//        return results;
-//    }
     @Override
     protected NDList forwardInternal(ParameterStore parameterStore, NDList inputs, boolean training, PairList<String, Object> params) {
         NDList results = new NDList();
@@ -150,22 +138,36 @@ private boolean withReward;
         if (withReward) {
             results.add(this.rewardHead.forward(parameterStore, new NDList(inputs.get(0)), training, params).get(0));
         }
-        NDArray input0StopGradient = inputs.get(0).stopGradient();
-        NDArray input1StopGradient = inputs.get(1).stopGradient();
-
-        results.add(this.policyHead.forward(parameterStore, new NDList(inputs.get(1).concat(input0StopGradient, 1)), training, params).get(0));
-        results.add(this.valueHead.forward(parameterStore, new NDList(inputs.get(2).concat(input1StopGradient, 1).concat(input0StopGradient, 1)), training, params).get(0));
+        results.add(this.policyHead.forward(parameterStore, new NDList(inputs.get(1)), training, params).get(0));
+        results.add(this.valueHead.forward(parameterStore, new NDList(inputs.get(2)), training, params).get(0));
         return results;
     }
+//    @Override
+//    protected NDList forwardInternal(ParameterStore parameterStore, NDList inputs, boolean training, PairList<String, Object> params) {
+//        NDList results = new NDList();
+//
+//        results.add(this.legalActionsHead.forward(parameterStore, new NDList(inputs.get(0)), training, params).get(0));
+//        if (withReward) {
+//            results.add(this.rewardHead.forward(parameterStore, new NDList(inputs.get(0)), training, params).get(0));
+//        }
+//        NDArray input0StopGradient = inputs.get(0).stopGradient();
+//        NDArray input1StopGradient = inputs.get(1).stopGradient();
+//
+//        results.add(this.policyHead.forward(parameterStore, new NDList(inputs.get(1).concat(input0StopGradient, 1)), training, params).get(0));
+//        results.add(this.valueHead.forward(parameterStore, new NDList(inputs.get(2).concat(input1StopGradient, 1).concat(input0StopGradient, 1)), training, params).get(0));
+//        return results;
+//    }
 
     @Override
     public void initializeChildBlocks(NDManager manager, DataType dataType, Shape... inputShapes) {
 
         legalActionsHead.initialize(manager, dataType, new Shape[]{inputShapes[0] });
         rewardHead.initialize(manager, dataType, new Shape[]{inputShapes[0] });
-        Shape combinedShape = combineShapes(inputShapes[0], inputShapes[1]);
+      //  Shape combinedShape = combineShapes(inputShapes[0], inputShapes[1]);
+        Shape combinedShape = inputShapes[1];
         policyHead.initialize(manager, dataType, new Shape[]{combinedShape});
-        combinedShape = combineShapes(combinedShape, inputShapes[2]);
+   //     combinedShape = combineShapes(combinedShape, inputShapes[2]);
+        combinedShape =  inputShapes[2] ;
         valueHead.initialize(manager, dataType, new Shape[]{combinedShape});
     }
 
