b942e0bb1006d0d0d9587e4a430a974e6b789b49
enpasos <info@enpasos.com>
2024-06-26 13:27:10 +0200

    testing

--- platform/src/main/java/ai/enpasos/muzero/platform/run/TestUnrollRulestate.java
+++ platform/src/main/java/ai/enpasos/muzero/platform/run/TestUnrollRulestate.java
@@ -80,7 +80,7 @@ public class TestUnrollRulestate {
     public Result run(int unrollsteps) {
         int epoch = networkIOService.getLatestNetworkEpoch();
 
-        timestepRepo.resetBoxAndSAndUOk();
+      //  timestepRepo.resetBoxAndSAndUOk();  // just for testing
         modelService.loadLatestModel(epoch).join();
 
 
