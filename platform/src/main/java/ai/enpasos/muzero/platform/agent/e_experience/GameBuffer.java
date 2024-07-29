9f0c862693a2feba6adb7746966deec9fd9fa6f2
enpasos <info@enpasos.com>
2024-07-29 14:51:30 +0200

    testing

--- platform/src/main/java/ai/enpasos/muzero/platform/agent/e_experience/GameBuffer.java
+++ platform/src/main/java/ai/enpasos/muzero/platform/agent/e_experience/GameBuffer.java
@@ -737,7 +737,7 @@ public class GameBuffer {
         List<IdProjection3> idProjectionsKnown = idProjections.stream().filter(idProjection3 -> idProjection3.getBox() > 0).collect(Collectors.toList());
         log.debug("getIdsRelevantForTraining: 1c");
 
-        List<IdProjection3> idProjectionsUnknownAndTrainable = idProjectionsUnknown.stream().filter(p -> p.getTrainable()).collect(Collectors.toList());
+        List<IdProjection3> idProjectionsUnknownAndTrainable = idProjectionsUnknown.stream().filter(p -> unrollSteps == 1 || p.getTrainable()).collect(Collectors.toList());
         log.debug("getIdsRelevantForTraining: 2");
 
         double k = 0.2; // TODO: configurable
