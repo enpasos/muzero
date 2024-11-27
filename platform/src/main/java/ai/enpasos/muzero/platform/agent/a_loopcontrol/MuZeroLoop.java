2ab52b2d2f88eb57c34d16302e853845e391b757
enpasos <info@enpasos.com>
2024-11-27 11:29:28 +0100

    testing

--- platform/src/main/java/ai/enpasos/muzero/platform/agent/a_loopcontrol/MuZeroLoop.java
+++ platform/src/main/java/ai/enpasos/muzero/platform/agent/a_loopcontrol/MuZeroLoop.java
@@ -165,7 +165,7 @@ public class MuZeroLoop {
         bufferGames.forEach(g -> g.getEpisodeDO().getTimeSteps().forEach(TimeStepDO::memorizeNormedSampleError));
 
         ruleTrain2(durations, unrollSteps);
-        //playService.uOkAnalyseGames(bufferGames,  false, unrollSteps, true);
+        playService.uOkAnalyseGames(bufferGames,  false, unrollSteps, true);
 
         // set maxSampleErrorChange from all time steps
         nonTrainedGames.forEach(g -> {
