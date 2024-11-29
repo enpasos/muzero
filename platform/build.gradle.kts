587e4436c5da8327cfb070bdede1a263c413aa05
enpasos <info@enpasos.com>
2024-11-29 19:42:10 +0100

    testing

--- platform/build.gradle.kts
+++ platform/build.gradle.kts
@@ -37,6 +37,11 @@ dependencies {
 
 
     implementation(libs.bundles.djl)
+    runtimeOnly(libs.djl.pytorch.native.cu124) {
+        artifact {
+            classifier = "win-x86_64"
+        }
+    }
 
     implementation(libs.gson)
     implementation(libs.jackson.databind)
