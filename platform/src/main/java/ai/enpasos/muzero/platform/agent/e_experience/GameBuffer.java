835fed94503f7d2bae7ce7419da063f7f82e091f
enpasos <info@enpasos.com>
2024-12-11 23:56:50 +0100

    testing

--- platform/src/main/java/ai/enpasos/muzero/platform/agent/e_experience/GameBuffer.java
+++ platform/src/main/java/ai/enpasos/muzero/platform/agent/e_experience/GameBuffer.java
@@ -237,7 +237,7 @@ public class GameBuffer {
         games.forEach(g -> g.getEpisodeDO().setGame(g));
 
         List<TimeStepDO> tsList = games.stream().map(g -> g.getEpisodeDO().getTimeSteps()).flatMap(List::stream)
-                .filter(ts -> ts.needsTraining(historyReliable, 30)).collect(Collectors.toList());
+                .filter(ts -> ts.needsTraining(historyReliable, 1)).collect(Collectors.toList());
         return tsList;
     }
 
